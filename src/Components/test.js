0d907a8d4fef16cd480de9699a924117ac1b95ab
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2021-03-17 21:39:24 +0530

    jordan

--- src/Components/test.js
+++ src/Components/test.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+const testthedash = () => {
+    return(
+        <div>
+            the don
+        </div>
+    )
+}
 
 function test() {
-    
+    return(
+        <div>
+            <h1>jordan the morgan</h1>
+        </div>
+    )
 }
 
 export default test
